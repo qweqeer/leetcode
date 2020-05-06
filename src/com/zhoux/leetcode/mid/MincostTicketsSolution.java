faa03323187438b5f28aebbdc55bddcd2e7d8f8b
xiangxiang <32702306+qweqeer@users.noreply.github.com>
2020-05-07 00:04:44 +0800

    最小票价

---
+++ src/com/zhoux/leetcode/mid/MincostTicketsSolution.java
@@ -0,0 +1,14 @@
+package com.zhoux.leetcode.mid;
+
+public class MincostTicketsSolution {
+
+    /**
+     * https://leetcode-cn.com/problems/minimum-cost-for-tickets/comments/
+     * 最低票价
+     */
+
+    public int mincostTickets(int[] days, int[] costs) {
+
+        return 0;
+    }
+}
