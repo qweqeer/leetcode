ef6cb35b49b009e1148cc215c6fd490809c2e889
xiangxiang <32702306+qweqeer@users.noreply.github.com>
2020-05-14 22:26:13 +0800

    待提升

--- src/com/zhoux/leetcode/mid/SingleNumberArraySolution.java
+++ src/com/zhoux/leetcode/mid/SingleNumberArraySolution.java
@@ -4,20 +4,22 @@ public class SingleNumberArraySolution {
     /**
      * 计算数组中出现一次的数字(其他数字都出现3次)
      * https://leetcode-cn.com/problems/single-number-iii/
-     * 解法：当一个数出现三次的时候抵消结果为0
-     * 有限状态机
-     * 第一次出现时使用a_bit保存结果，b_bit归零
-     * 第二次出现时使用b_bit保存数字，a_bit归零
-     * 第三次出现时使用a_bit与b_bit均归零
+     *
+     *
      */
 
     public static int[] singleNumber(int[] nums) {
-
+        int def = 0;
+        for (int num : nums) {
+            def=def^num;
+        }
+        int leftPoint=0,rightPoint=0;
+        //TODO 升入了解位运算的特点
         return null;
     }
 
     public static void main(String[] args) {
-        System.out.println(singleNumber(new int[]{2,2,3,2}));
+        System.out.println(5&-5);
     }
 
 }
