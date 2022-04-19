05b5c92bb6b37616e48c4d820b7b8db1460f6386
xiangxiang <32702306+qweqeer@users.noreply.github.com>
2022-04-19 23:39:41 +0800

    贪心算法，区间重叠

---
+++ src/leetcode/editor/cn/[253]会议室 II.java
@@ -0,0 +1,43 @@
+//给你一个会议时间安排的数组 intervals ，每个会议时间都会包括开始和结束的时间 intervals[i] = [starti, endi] ，返回 
+//所需会议室的最小数量 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：intervals = [[0,30],[5,10],[15,20]]
+//输出：2
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：intervals = [[7,10],[2,4]]
+//输出：1
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= intervals.length <= 104 
+// 0 <= starti < endi <= 106 
+// 
+// Related Topics 贪心 数组 双指针 排序 堆（优先队列） 
+// 👍 427 👎 0
+
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        //计算同一时刻最大并行的会议
+        //查分数组变种
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
